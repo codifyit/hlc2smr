f372b9712767a831b21bc96be76f76207574a063
codifyit <rmenmar264@g.educaand.es>
2024-12-02 10:00:12 +0100

    First form example

--- tema1/forms/register.php
+++ tema1/forms/register.php
@@ -1,8 +1,9 @@
 <html>
+<head>
+    <title>First form example with PHP server code</title>
+</head>
 <body>
-
-Welcome <?php echo $_GET["fname"]." ". $_GET["lname"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
-
+    Welcome <?php echo $_GET["fname"] . " " . $_GET["lname"]; ?><br>
+    Your email address is: <?php echo $_GET["email"]; ?>
 </body>
 </html>
